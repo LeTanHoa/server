43e8672dbf5536a41dbfdbf21c9968da75da89ca
LeTanHoa <96413637+LeTanHoa@users.noreply.github.com>
2026-05-05 20:25:19 +0700

    Simplify CORS origin configuration

--- src/index.js
+++ src/index.js
@@ -14,12 +14,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: [
-    "https://musicut.vercel.app"
-  ],
+  origin: "https://musicut.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+
+app.options("*", cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
